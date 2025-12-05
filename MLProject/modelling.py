d199371caf05816bb0780e8b7ae87471ce9ec012
MasayuSarahAmelia <163170723+MasayuSarahAmelia@users.noreply.github.com>
2025-12-05 18:06:12 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-
-mlflow.set_experiment("Heart Disease Prediction")
+# Simpan experiment secara lokal (bukan ke server)
+mlflow.set_tracking_uri("file:./mlruns")
 
 data = pd.read_csv("heartdisease_preprocessing.csv")
 
@@ -17,25 +17,20 @@ y = data["target"]
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Aktifkan autolog (syarat basic)
-mlflow.sklearn.autolog()
-
-# Experiment run MLflow
+# Mulai run manual (bukan autolog)
 with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
-    # pred = model.predict(X_test)
-    # acc = accuracy_score(y_test, pred)
+    pred = model.predict(X_test)
+    acc = accuracy_score(y_test, pred)
 
-    # # Logging manual hanya untuk metric tambahan
-    # mlflow.log_metric("accuracy_manual", acc)
-    # mlflow.log_param("n_estimators", 100)
-    # mlflow.log_param("random_state", 42)
+    # Logging manual
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("random_state", 42)
 
-    # # Simpan artefak model
-    # mlflow.sklearn.log_model("model")
+    # Simpan model
+    mlflow.sklearn.log_model(model, "model")
 
-    # print("Accuracy:", acc)
-
-    print("Training selesai (basic).")
+    print("Training selesai.")
