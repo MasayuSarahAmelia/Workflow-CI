5703a37a67498bb438a1e13e957dbf203d2e281c
MasayuSarahAmelia <163170723+MasayuSarahAmelia@users.noreply.github.com>
2025-12-05 17:59:54 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-mlflow.set_tracking_uri("http://localhost:5000")
+
 mlflow.set_experiment("Heart Disease Prediction")
 
 data = pd.read_csv("heartdisease_preprocessing.csv")
